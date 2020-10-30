ac47cbb47c0ea953ed9cdc783b928186be2f122c
Choharim <jhl8586@naver.com>
2020-10-30 13:46:31 +0900

    Add clock

---
+++ clock.js
@@ -0,0 +1,20 @@
+const clockContainer = document.querySelector(".js_clock");
+const clockTitle = clockContainer.querySelector("h1");
+
+
+function getTime(){ 
+  
+  const date = new Date();
+
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+
+  clockTitle.innerText = `${hours}:${minutes}`;
+}
+
+function init(){
+
+  getTime();
+
+}
+init();

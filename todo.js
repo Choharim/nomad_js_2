51791198cc09f8fa9ca03555dded2f1ebe702b36
Choharim <jhl8586@naver.com>
2020-11-01 23:05:20 +0900

    finish todolist

--- todo.js
+++ todo.js
@@ -3,8 +3,22 @@ toDoInput = toDoForm.querySelector("input"),
 toDoList = document.querySelector(".js-toDoList");
 
 
-const TODOS_LS = "toDos",
-toDos = [];
+const TODOS_LS = "toDos";
+let toDos = [];
+
+
+function deleteToDo(event){
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function (toDo){
+    return toDo.id !==parseInt(li.id);
+    
+  });
+  toDos = cleanToDos
+  saveToDos();
+  
+}
 
 function saveToDos(){
   localStorage.setItem(TODOS_LS,JSON.stringify(toDos));
@@ -16,7 +30,7 @@ function paintToDo(text){
   const span = document.createElement("span");
   const newId = toDos.length + 1;
   delBtn.innerText = "X";
- 
+  delBtn.addEventListener("click",deleteToDo);
   span.innerText = text;
   li.appendChild(span);
   li.id = newId;
